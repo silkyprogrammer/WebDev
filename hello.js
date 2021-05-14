98e9dd2e1d2162a4cb310120ffa15fcb9d3e8838
silkyprogrammer <hpalappe@gmail.com>
2021-05-14 11:58:40 +0530

    latest notes

---
+++ hello.js
@@ -0,0 +1,17 @@
+let myNumber = 10;
+let herNumber = 11;
+herNumber--;
+print(herNumber)
+
+const near = 1;
+// near = 2 ERROR
+// near += 1 ERROR
+
+const pi = 3.141567
+
+var acctBalance = 0.0; // dont use var anymore. It's the old way.
+acctBalance += 1;
+
+const boilingPointC = 100;
+const boilingPointF = 212;
+
