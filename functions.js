39b2a5acf15a1b2d2b935db7a8ce9e19c66dc19f
silkyprogrammer <hpalappe@gmail.com>
2021-05-23 18:14:12 +0530

    updates till 23/May

---
+++ functions.js
@@ -0,0 +1,62 @@
+
+
+
+//For one dice, what if we want 6 times, to avoid duplication we use functions.
+
+function rollDice() {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice)
+}
+for (let i = 0; i < 10; i++) {
+    rollDice();
+}
+
+
+function printHeart() {
+    console.log("<3")
+}
+
+printHeart()
+
+
+function rant(message) {
+    let msg = message.toUpperCase();
+    console.log(msg)
+    console.log(msg)
+    console.log(msg)
+}
+
+
+function isSnakeEyes(roll1, roll2) {
+    if (parseInt(roll1) === 1 && parseInt(roll2) === 1) {
+        console.log("Snake Eyes!")
+    } else {
+        console.log("Not Snake Eyes!")
+    }
+}
+
+function multiply(arg1, arg2) {
+    if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
+        return false
+    }
+    let result = arg1 * arg2;
+    return result;
+
+}
+
+
+function isShortsWeather(num) {
+    if (parseInt(num) < 75) {
+        return false;
+
+    }
+    return true;
+}
+
+//DID NOT CLEAR THE CHECKS - Work on it again.
+function lastElement(arr) {
+    if (arr.length === 0) {
+        return null
+    }
+    console.log(arr[arr.length - 1]);
+}
--- todolist.js
+++ todolist.js
@@ -1,7 +1,31 @@
 let input = prompt("What would you like to do");
+const todos = ["Collect Pens", "Get Groceries"];
 while (input !== "quit" && input !== "q") {
+    if (input === "list") {
+        console.log("****************************")
+        for (let i = 0; i < todos.length; i++) {
+            const element = todos[i];
+            console.log(`${i}: ${element}`)
+        }
+        console.log("****************************")
+    } else if (input === "new") {
+        const newTodo = prompt("Enter new Todo");
+        todos.push(newTodo)
+        console.log(`${newTodo} todo added to list`);
+    } else if (input === "delete") {
+        const indexString = prompt("OK, enter the index to delete");
+        const index = parseInt(indexString)
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1)
+            console.log(`${deleted} - deleted from the list`)
+        } else {
+            console.log("Invalid Index")
+        }
+
+    }
     input = prompt("What would you like to do");
 }
-console.log("OK! Quitting Todo")
+console.log("OK! Quitting Todo");
 
-const todos = ['Collect Pens'];
+
+
